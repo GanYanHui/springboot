754d26ef907798e98fbbc05ddb937029ad679843
甘延辉 <892963616@qq.com>
2022-04-05 19:54:01 +0800

    添加vue目录下的文件

---
+++ vue/src/components/Header.vue
@@ -0,0 +1,54 @@
+<template>
+  <div style="line-height: 60px; display: flex">
+    <div style="flex: 1;">
+      <span :class="collapseBtnClass" style="cursor: pointer" @click="collapse"></span>
+
+      <!-- 页签 -->
+      <el-breadcrumb separator="/" style="display: inline-block; margin-left: 10px">
+        <el-breadcrumb-item :to="'/'">{{ $route.name }}</el-breadcrumb-item>
+
+      </el-breadcrumb>
+    </div>
+
+    <el-dropdown style="width: 150px; cursor: pointer; text-align: right">
+      <div style="display: inline-block">
+        <img src="" alt="" style="width: 30px; border-radius: 50%; position: relative; top: 10px; right: 5px">
+        <span>{{ user.username }}<i class="el-icon-caret-bottom" style="margin-left: 5px"></i></span>
+      </div>
+      <el-dropdown-menu slot="dropdown" style="width: 70px; text-align: center">
+        <el-dropdown-item style="font-size: 14px; padding: 5px 0">
+          <router-link to="/person">个人信息</router-link></el-dropdown-item>
+        <el-dropdown-item style="font-size: 14px; padding: 5px 0">
+          <span style="text-decoration: none" @click="logout">退出</span>
+        </el-dropdown-item>
+      </el-dropdown-menu>
+    </el-dropdown>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Header",
+  props: {
+    collapseBtnClass: String,
+    collapse: Function
+  },
+  data(){
+    return {
+      user: localStorage.getItem("user") ? JSON.parse(localStorage.getItem("user")) : {}
+    }
+  },
+  methods: {
+    logout() {
+      this.$router.push("/login")
+      localStorage.removeItem("user")
+      this.$message.success("退出成功")
+    }
+  }
+
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ vue/src/router/index.js
@@ -0,0 +1,43 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    component: () => import('../views/Manage.vue'),
+    redirect: "/home",
+    children: [
+      { path: 'home', name: '首页', component: () => import('../views/Home.vue')},
+      { path: 'user', name: '用户管理', component: () => import('../views/User.vue')},
+      { path: 'person', name: '个人信息', component: () => import('../views/Person.vue')},
+    ]
+  },
+  {
+    path: '/about',
+    name: 'about',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: () => import('../views/Login.vue')
+  },
+  {
+    path: '/register',
+    name: 'Register',
+    component: () => import('../views/Register.vue')
+  }
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
+
+export default router
