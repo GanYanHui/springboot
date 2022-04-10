19070808eed040ccc2f96545b3933e1ec988a84a
甘延辉 <892963616@qq.com>
2022-04-10 22:17:55 +0800

    21

---
+++ src/main/java/com/gyh/springboot/mapper/DictMapper.java
@@ -0,0 +1,7 @@
+package com.gyh.springboot.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.gyh.springboot.entity.Dict;
+
+public interface DictMapper extends BaseMapper<Dict> {
+}
