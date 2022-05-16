f636497384439c4b31b46c4ff28b5ff15fa1edbe
甘延辉 <892963616@qq.com>
2022-05-16 13:06:17 +0800

    新增：新增视频、文章界面(没写)

---
+++ vue/src/views/front/Article.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1>该功能暂未开放</h1>
+</template>
+
+<script>
+export default {
+  name: "Article"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ vue/src/views/front/Video.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1>该功能暂未开放</h1>
+</template>
+
+<script>
+export default {
+  name: "Video"
+}
+</script>
+
+<style scoped>
+
+</style>
